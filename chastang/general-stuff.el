f20e0dcb8947f95ecd5bbc07323006b21adfa7ce
Julien Chastang <julien.c.chastang@gmail.com>
2013-02-18 09:57:20 -0700

    autocomplete

--- chastang/general-stuff.el
+++ chastang/general-stuff.el
@@ -4,3 +4,10 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
+;; auto-complete
+(require 'popup)
+(require 'auto-complete-config)
+(ac-config-default)
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+(ac-set-trigger-key "TAB")
+
