0198749dbb483b84a0d87518670da17c380a08a6
Julien Chastang <julien.c.chastang@gmail.com>
2013-02-18 04:45:15 -0700

    mode line stuff

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
 
 (load "safe-load" nil t)
 (safe-load "general-stuff" nil t)
+(safe-load "mode-line" nil t)
 (safe-load "bubbleberry-theme" nil t)
 (safe-load "clojure-stuff" nil t)
 (safe-load "org-stuff" nil t)
