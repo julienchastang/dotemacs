1fa3b2101ebf507da46cc310bedec87b0465b372
Julien Chastang <julien.c.chastang@gmail.com>
2013-02-19 23:50:22 -0700

    smex and other stuff

--- chastang/general-stuff.el
+++ chastang/general-stuff.el
@@ -1,7 +1,9 @@
 (menu-bar-mode -1)
 
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
+
 ;; Undo tree support
-(require 'undo-tree)
 (global-undo-tree-mode)
 
 ;; auto-complete
@@ -11,4 +13,12 @@
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 (ac-set-trigger-key "TAB")
 
+;; parentheses
 (show-paren-mode 1)
+
+;; smex
+(smex-initialize) 
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
