ebfdee3af315cc97bedb5141f3ff1d887ccb43e6
Julien Chastang <julien.c.chastang@gmail.com>
2013-03-17 13:03:20 -0600

    pretty fn

---
+++ chastang/clojure-stuff.el
@@ -0,0 +1,7 @@
+(defun esk-pretty-fn ()
+  (font-lock-add-keywords nil `(("(\\(\\<fn\\>\\)"
+				 (0 (progn (compose-region (match-beginning 1)
+							   (match-end 1)
+							   "\u0192"
+							   'decompose-region)))))))
+(add-hook 'clojure-mode-hook 'esk-pretty-fn)
