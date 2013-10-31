49a4fa8614dab6fae2b2944df89188c8a03d745c
Julien Chastang <julien.c.chastang@gmail.com>
2013-10-31 16:59:40 -0600

    rename file and buffer function

--- chastang/general-stuff.el
+++ chastang/general-stuff.el
@@ -51,3 +51,20 @@
 ;; snippets
 (require 'yasnippet)
 (yas-global-mode 1)
+
+
+;; rename file and buffer
+;; http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer/
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
