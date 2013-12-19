3b14961d7a6c6e02e0e7009f34ccea63f5ae5dd4
Julien Chastang <julien.c.chastang@gmail.com>
2013-12-18 23:28:14 -0700

    code block syntax highlighting

--- chastang/org-stuff.el
+++ chastang/org-stuff.el
@@ -10,3 +10,6 @@
       '((?A . (:foreground "red"))
         (?B . (:foreground "#00BB00" ))
         (?C . (:foreground "blue" ))))
+
+;; code block syntax highlighting
+(setq org-src-fontify-natively t)
