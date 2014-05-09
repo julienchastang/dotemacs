b2b5cca728dd2a3bd7628ce951c940d869e08ddf
Julien Chastang <julien.c.chastang@gmail.com>
2014-05-09 15:30:18 -0600

    trying to make orgmode look better

--- chastang/org-stuff.el
+++ chastang/org-stuff.el
@@ -1,15 +1,20 @@
+(define-key global-map "\C-ca" 'org-agenda)
+
 (setq org-todo-keywords
-             '((sequence "➜" "✈" "|" "✔" )))
+             '((sequence "TODO" "PROG" "STCK" "DONE" )))
 
 (setq org-todo-keyword-faces
-      (quote (("➜" :foreground "red"       :weight bold)
-              ("✈" :foreground "goldenrod" :weight bold)
-              ("✔" :foreground "forest green"))))
+      (quote (("TODO" :foreground "#DB2929" :background "#1A1A1A")
+              ("PROG" :foreground "#D08800" :background "#1A1A1A")
+              ("STCK" :foreground "#375EB7" :background "#1A1A1A")
+              ("DONE" :foreground "#43A243" :background "#1A1A1A"))))
+
+(setq org-tag-faces '(("URGENT" :background "#DB2929")))
 
 (setq org-priority-faces
       '((?A . (:foreground "red"))
-        (?B . (:foreground "#00BB00" ))
-        (?C . (:foreground "blue" ))))
+        (?B . (:foreground "#D08800" ))
+        (?C . (:foreground "#43A243" ))))
 
 ;; code block syntax highlighting
 (setq org-src-fontify-natively t)
